5f9b744ec2983eb968f75aa17320dcdce319bf74
Filip Bilinski <F.K.Bilinski@student.tudelft.nl>
2023-05-20 11:35:31 +0200

    Changed offset to be calculated based on maxarg in a slice

--- server/server.py
+++ server/server.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from db_handler import db_handler
 import pymongo
+import cv2
+import time
 
 APP = Flask(__name__)
 
-#cred = credentials.Certificate('key.json')
-#app = firebase_admin.initialize_app(cred)
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = db_handler(client, "RP")
 
@@ -22,13 +22,38 @@ max_frequency = 20500
 
 interval = 0.1
 sample_rate = 44100
-chirp_amount = 40
-# amount of chirps that are ignored, since some of the last chirps dont work
-chirp_sample_offset = 441
+chirp_amount = 504
 
 interval_rate = sample_rate * interval
 
-def create_spectrogram(array, filename, cut_offset):
+def create_spectrogram(array, cut_offset):
+    f, t, Sxx = spectrogram(array, 44100, window=hann(256, sym=False))
+
+    
+    indecies = np.where((f > min_frequency) & (f < max_frequency))
+
+    
+    time_segments = t.shape[0] - cut_offset
+    Sxx = Sxx[indecies, cut_offset:time_segments]
+    Sxx = Sxx[0]
+    f = f[indecies]
+    t = t[cut_offset:time_segments]
+
+    figure = plt.pcolormesh(t, f, Sxx, shading='gouraud')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.savefig("temp.jpg")
+    plt.clf()
+
+    time.sleep(0.2)
+
+    rgb = cv2.imread("temp.jpg")
+    rgb = rgb[59:428, 80:579]
+    rgb = cv2.resize(rgb, (int(rgb.shape[0]/2), int(rgb.shape[1]/2)))
+    
+    return rgb
+
+def debug_spectrogram(array, filename, cut_offset):
     f, t, Sxx = spectrogram(array, 44100, window=hann(256, sym=False))
 
     
@@ -64,27 +89,36 @@ def add_room():
     room_audio = room_data['audio']
     
     
-    data = {
-        u'building': building_label,
-        u'room': room_label,
-        u'audio': "Currently stored locally",
-    }
-
-    db.add_entry(building_label, data)
+    
 
     counter = 0
     np_arr = np.asarray(room_audio, dtype=np.int16)
 
     #create_spectrogram(np_arr[0], "whole.jpg")
 
-    for i in range(1, chirp_amount - 2):
-        start_rate = int(i * interval_rate - chirp_sample_offset)
+    for i in range(2, chirp_amount - 2):
+
+        start_rate = int(i * interval_rate)
         sliced = np_arr[0,start_rate:(int(start_rate + interval_rate))]
-        create_spectrogram(sliced, 'tarck' + str(counter) + '.jpg', 4)
+        
+
+        start_rate = int(i * interval_rate + np.argmax(sliced))
+        sliced = np_arr[0,start_rate:(int(start_rate + interval_rate))]
+        
+        if i < 20:
+            debug_spectrogram(sliced, 'tarck_cut' + str(counter) + '.jpg', 5)
+            debug_spectrogram(sliced, 'tarck' + str(counter) + '.jpg', 0)
         counter += 1
-    
-    # filename = doc_ref.id + ".wav"
-    # write(filename, 44100, arr)
+
+        rgb = create_spectrogram(sliced, 5)
+
+        data = {
+        u'building': building_label,
+        u'room': room_label,
+        u'audio': rgb.tolist()
+        }
+
+        db.add_entry(building_label, data)
 
 
     return 'OK'
